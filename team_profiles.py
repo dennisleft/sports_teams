2ce000da7fd30bd9ab8c5572c9f6156fd87f34e7
Dennis Iruguthu <dennisiruguthu@gmail.com>
2019-01-21 16:20:02 +0300

    simmons

--- team_profiles.py
+++ team_profiles.py
@@ -3,6 +3,7 @@
 # Manager - name, country, age
 # Player - name, squad number, position, age, nationality
 
+
 class Manager:
 
     def __init__(self, name, country, birth_year):
@@ -18,10 +19,27 @@ class Manager:
         return f'\nName: {self.name}\nNationality: {self.country}\nAge: {self.age()}\n'
 
 
-class Team(Manager):
+class Player(Manager):
 
-    def __init__(self, name, sport, country, league, colours, manager, stadium, aka, town, birth_year):
+    def __init__(self, name, team, country, jersey, position, birth_year):
         super().__init__(name, country, birth_year)
+        self.team = team
+        self.jersey = jersey
+        self.position = position
+        #if self.name == Team.name:
+            #Team.player_roster.append(self.name)
+
+    def player_profile(self):
+        return f'{Manager.profile(self)}Team: {self.team}\nJersey: {self.jersey}\nPosition: {self.position}'
+
+
+class Team(Player):
+
+    def __init__(self, name, sport, country, league, colours, manager, stadium, aka, town, birth_year,
+                 player_roster=[]):
+        self.name = name
+        self.country = country
+        self.birth_year = birth_year
         self.sport = sport
         self.league = league
         self.colours = colours
@@ -29,17 +47,19 @@ class Team(Manager):
         self.stadium = stadium
         self.aka = aka
         self.town = town
+        self.player_roster = player_roster
+
+    def players(self):
+        for i in self.player_roster:
+            pass
 
     def profile(self):
         return f'\nTEAM PROFILE\nName: {self.name}\nSport: {self.sport}\nCountry: {self.country}\n' \
                f'Founded: {self.birth_year}\nLeague: {self.league}\nStadium: {self.stadium}\n' \
                f'Town: {self.town}\nAKA: {self.aka}\nCurrent Manager-> {self.manager}'
 
-
-class Player(Manager):
-    pass
-
-
+    def add_to_team(self):
+        pass
 # Manager
 sarri = Manager('Maurizio Sarri', 'Italian', 1959)
 valverde = Manager('Ernesto Valverde', 'Spanish', 1964)
@@ -57,3 +77,7 @@ sixers = Team('Philadelphia 76ers', 'Basketball', 'USA', 'NBA', 'Blue, White and
               brown.profile(), 'Wells Fargo Center', 'Sixers', 'Philadelphia', 1946)
 
 print(chelsea.profile(), barca.profile(), sixers.profile())
+
+# players
+simmons = Player('Ben Simmons', 'Philadelphia 76ers', 'Australia', 25, 'Point Guard', 1996)
+print(simmons.player_profile())
